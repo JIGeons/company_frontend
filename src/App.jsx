8ddc7dcb8bab79e32e82b82f99b01cc5768e9563
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-03-16 04:55:02 +0900

    Ch1-6 회사 페이지 생성 후 App.jsx 경로 설정(createBrowserRouter)
    createBrowserRouter로 중첩 라우팅 및 Outlet을 사용하여 불필요한 랜더링 최소화

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,63 @@
 import './App.css'
-import {BrowserRouter} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
 // Components
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import Footer from "./Components/Footer/Footer.jsx";
+import MainPage from "./Page/MainPage/MainPage.jsx";
+import About from "./Page/About/About.jsx";
+import Leadership from "./Page/Leadership/Leadership.jsx";
+import Board from "./Page/Board/Board.jsx";
+import Services from "./Page/Services/Services.jsx";
+import Contact from "./Page/Contact/Contact.jsx";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  )
+}
+
+// createBrowserRouter를 사용해 중첩 라우팅을 구성
+// -> 페이지 이동 시 Navbar, Footer는 재랜더링 없이 Outlet만 렌더링
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { /* default 페이지 */
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      }
+    ]
+  }
+]);
 
 function App() {
-
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Footer />
-    </BrowserRouter>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
