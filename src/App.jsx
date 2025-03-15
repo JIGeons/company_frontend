d579990d9f93d712e57687c083980e0c9f326c84
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-03-16 04:25:01 +0900

    ch1-5 하단 바(Footer) 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
-import Navbar from "./Components/Navbar/Navbar.jsx";
-
 import {BrowserRouter} from "react-router-dom";
+
+// Components
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   )
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
-    <nav className="fixed top-0 left-5 w-full bg-white text-black p-4 shadow-lg z-50">
+    <nav className="fixed top-0 left-0 w-full bg-white text-black p-4 shadow-lg z-50">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-xl lg:text-2xl font-bold lg:ml-12 lg:mr-8"><a href="/">ABC Company</a></h1>
 
