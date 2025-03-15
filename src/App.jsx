a878b62c1cb79d0697167b4da134b52eccb3dffa
JIGeons <rlrjtlrl@skuniv.ac.kr>
2025-03-16 04:00:17 +0900

    ch1 - 2~4. 네비게이션 바(Navbar) 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import Navbar from "./Components/Navbar/Navbar.jsx";
+
+import {BrowserRouter} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
   )
 }
 
